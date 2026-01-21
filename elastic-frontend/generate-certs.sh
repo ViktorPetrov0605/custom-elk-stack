aad8165c8dca395b986b616e0ed82803c35202df
ViktorPetrov0605 <viktorpetrov0605@gmail.com>
2026-01-21 10:19:01 +0200

    Updated certificate generation script to check for unzip dependency

--- elastic-frontend/generate-certs.sh
+++ elastic-frontend/generate-certs.sh
@@ -5,6 +5,12 @@ CA_ZIP="elastic-stack-ca.zip"
 CERTS_ZIP="node-certs.zip"
 OUTPUT_DIR="./certs"
 
+# Check if the unzip command is availible. 
+if ! command -v unzip &> /dev/null; then
+    echo "unzip could not be found"
+    exit 1
+fi
+
 # Create directory structure
 mkdir -p $OUTPUT_DIR
 
@@ -19,11 +25,11 @@ echo "--- Generating Node Certificates ---"
 cat <<EOF > $OUTPUT_DIR/instances.yml
 instances:
   - name: es-frontend
-    dns: ["es-frontend", "localhost"]
-    ip: ["127.0.0.1"]
-  - name: es-backend # Replace with Backend Server cluster.name
-    dns: ["es-backend"] # Same as above
-    ip: ["<BACKEND_SERVER_IP>"] # Replace with Backend Server IP
+    dns: [es-frontend, localhost]
+    ip: [127.0.0.1]
+  - name: cluster-bix-1 # Replace with Backend Server cluster.name
+    dns: [cluster-bix-1] # Same as above
+    ip: [10.4.4.87] # Replace with Backend Server IP
 EOF
 
 # Generate the certificates using the CA created above
@@ -33,7 +39,6 @@ docker run --rm -v $(pwd)/$OUTPUT_DIR:/certs \
   --in /certs/instances.yml --out /certs/bundle.zip --pass "" --silent
 
 # Unzip the results
-apt-get install unzip -y || yum install unzip -y
 unzip $OUTPUT_DIR/bundle.zip -d $OUTPUT_DIR/
 
 echo "--- Cleaning up ---"
